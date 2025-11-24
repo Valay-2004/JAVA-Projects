295f0aefcecaee1565350cd3b617b60f0ceb2806
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-24 11:14:06 +0000

    "moved files to a parent folder"

--- StudentGMS/StudentGMS/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //  This is my first project after long time
-        // What we are going to make
-        // A console-based application that manages student grades,
-        // allowing teachers to add, view, and calculate averages
-        // while handling various error scenarios.
-    }
-}
