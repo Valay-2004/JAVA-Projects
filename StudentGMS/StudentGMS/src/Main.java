2d1322d9244f9277b9d3307c7d666b0eb1ef135c
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-24 11:11:06 +0000

    "Initial Commit : Added new files for Student and Main and Exception classes"

---
+++ StudentGMS/StudentGMS/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //  This is my first project after long time
+        // What we are going to make
+        // A console-based application that manages student grades,
+        // allowing teachers to add, view, and calculate averages
+        // while handling various error scenarios.
+    }
+}
---
+++ StudentGMS/StudentGMS/src/Student.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Student {
+
+        private String name;
+        private String id;
+        private List<Double> grades;
+
+        // Constructor template
+        // Getters and setters
+        // Method to add grade
+        // Method to calculate average
+        // toString method
+}
