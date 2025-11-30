b94a239b0e43240754f087ea0dd47c2766803bc3
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2025-11-30 20:28:21 +0530

    "Very hard to deal with this much methods without tutorial :<"

--- StudentGMS/src/GradeManager.java
+++ StudentGMS/src/GradeManager.java
@@ -11,21 +11,26 @@ public class GradeManager {
     }
 
     public void addStudent(String name, String id) {
-        // Create new student and add to map
-        // Use student ID as the key
+        Student s = new Student(name, id);
+        students.put(id, s);
     }
 
     public void addGradeToStudent(String studentId, double grade)
             throws StudentNotFoundException, InvalidGradeException {
         // 1. Find student by ID
-        // 2. If student exists, add grade to their record
-        // 3. If student doesn't exist, throw StudentNotFoundException
+        Student s = students.get(studentId);
+        // 2. If student doesn't exist, throw StudentNotFoundException
+        if(s == null) throw new StudentNotFoundException("Unable to find the student");
+        // 3. If student exists, add grade to their record
+        if(grade < 0 || grade > 100) throw new InvalidGradeException("Grade range should be between 0 to 100");
+        s.addGrade(grade);
     }
 
     public Student getStudent(String id) throws StudentNotFoundException {
         // 1. Look up student in the map
         // 2. Return student if found
         // 3. Throw exception if not found
+        return null;
     }
 
     public void displayAllStudents() {
