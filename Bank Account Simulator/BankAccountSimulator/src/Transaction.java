cf16a4095805ce8995dc41e8ff0d470669d118f1
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2025-12-26 22:59:36 +0530

    Added Constructor, Getters and Setters | WIll add validation and other methods later good bye for now :)

--- Bank Account Simulator/BankAccountSimulator/src/Transaction.java
+++ Bank Account Simulator/BankAccountSimulator/src/Transaction.java
@@ -7,4 +7,51 @@ public class Transaction {
     String fromAccount;
     String toAccount;
 
+    public Transaction(String transactionType, String transactionTimeStamp, String fromAccount, String toAccount, BigDecimal transactionAmount) {
+        this.transactionType = transactionType;
+        this.transactionTimeStamp = transactionTimeStamp;
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.transactionAmount = transactionAmount;
+    }
+
+    public BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getTransactionTimeStamp() {
+        return transactionTimeStamp;
+    }
+
+    public void setTransactionTimeStamp(String transactionTimeStamp) {
+        this.transactionTimeStamp = transactionTimeStamp;
+    }
+
+    public String getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(String fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public String getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(String toAccount) {
+        this.toAccount = toAccount;
+    }
 }
