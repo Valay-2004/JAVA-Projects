7fac280a81f4ce7ed66e7d3b77f9e2c56ca769ad
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2025-12-26 22:54:16 +0530

    Initial Commit: Started Transaction

---
+++ Bank Account Simulator/BankAccountSimulator/src/Transaction.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+
+public class Transaction {
+    BigDecimal transactionAmount;
+    String transactionType;
+    String transactionTimeStamp;
+    String fromAccount;
+    String toAccount;
+
+}
