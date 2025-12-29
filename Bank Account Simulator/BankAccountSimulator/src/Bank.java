43c8fcff523d2c1bb9984a9b398194b87ccbff56
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2025-12-29 19:52:19 +0530

    Initial Commit - Creation of Bank class

---
+++ Bank Account Simulator/BankAccountSimulator/src/Bank.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+
+    private Map<String, Account> accounts;
+
+    public Bank(){
+        this.accounts = new HashMap<>();
+    }
+
+    public void addAccount(String name, Account.AccountType accountType){
+
+    }
+}
