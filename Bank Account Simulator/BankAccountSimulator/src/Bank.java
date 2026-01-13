fa030f50c4134fa01ace2c9cf6698b726235e4fb
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-01-13 16:44:04 +0530

    Add getAccount, transfer methods for working

--- Bank Account Simulator/BankAccountSimulator/src/Bank.java
+++ Bank Account Simulator/BankAccountSimulator/src/Bank.java
@@ -1,3 +1,6 @@
+import exceptions.*;
+
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +12,34 @@ public class Bank {
         this.accounts = new HashMap<>();
     }
 
-    public void addAccount(String name, Account.AccountType accountType){
+    public void addAccount(String accountNumber, String holderName, Account.AccountType accountType, BigDecimal initialBalance) {
+        Account account = new Account(initialBalance,accountNumber, holderName, accountType);
+        accounts.put(accountNumber, account);
+    }
 
+    public Account getAccount(String accountNumber) throws AccountNotFoundException {
+        if(!accounts.containsKey(accountNumber)){
+            throw new AccountNotFoundException("Account Number " + accountNumber + " was not found in our bank's records!");
+        }
+        return accounts.get(accountNumber);
+    }
+
+    public void transfer(String fromAccountNumber, String toAccountNumber, BigDecimal amount) throws AccountNotFoundException, InsufficientFundsException {
+        // 1. Get both account with null checks that throw exception
+        if(fromAccountNumber == null || toAccountNumber == null || amount == null){
+            throw new InvalidTransferException("Source, Destination, or Amount cannot be null!");
+        }
+        if(fromAccountNumber.equals(toAccountNumber)){
+            throw new InvalidTransferException("Source and Destination address cannot be same!");
+        }
+        if(amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidTransferException("Transfer amount must be positive value");
+        }
+        Account fromAccount = getAccount(fromAccountNumber);    // source
+        Account toAccount = getAccount(toAccountNumber);        // destination
+        // withdraw
+        fromAccount.withdraw(amount);
+        // deposit
+        toAccount.deposit(amount);
     }
 }
