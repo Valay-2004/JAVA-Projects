1c9b51f64e66bbf9ac326011a31393b9f5a5274d
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-01-14 17:38:25 +0530

    Main is same having the BankingApp start here

--- Bank Account Simulator/BankAccountSimulator/src/Main.java
+++ Bank Account Simulator/BankAccountSimulator/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bank Account Simulator Mini Project");
+        BankingApp bankingApp = new BankingApp();
+        bankingApp.start();
     }
 }
