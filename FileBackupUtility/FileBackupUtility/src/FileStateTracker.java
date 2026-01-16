890500ad26b7161756fdb26fcafcd20f2b3395bc
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-01-16 17:20:32 +0530

    Initial Commit -- Add initializeFile(for file and dir check) and hasFileChanged(check if file is updated through timestamp) methods

---
+++ FileBackupUtility/FileBackupUtility/src/FileStateTracker.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileStateTracker {
+    //Path for the file
+    private final Path manifestPath;
+
+    // Constructor
+    public FileStateTracker(String manifestFilePath) {
+        this.manifestPath = Paths.get(manifestFilePath);
+        initializeFile();
+    }
+
+    private void initializeFile() {
+        // check for the file
+        try {
+            Path parentDir = manifestPath.getParent();
+            if (parentDir != null && Files.notExists(parentDir)) {
+                Files.createDirectories(parentDir); // creating parent directory;
+            }
+            // create file if not exist
+            if (Files.notExists(manifestPath)) {
+                Files.createFile(manifestPath);
+            }
+            // do nothing as file already exists
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public boolean hasFileChanged(Path sourceFile) {
+        if(!Files.exists(sourceFile)) return true; // file was deleted so there was a change
+        try(BufferedReader reader = Files.newBufferedReader(manifestPath)){
+            String line;
+            while((line = reader.readLine()) != null){
+                if(line.isBlank()) continue;
+
+                // 2 limits to 2 parts (just in case someone has | is filepath)
+                String[] p = line.split("\\|", 2);
+                if(p.length < 2) continue; // skip any malformed lines
+                String path = p[0];
+
+                if(path.equals(sourceFile.toString())){
+                    try {
+                        long lastKnownTime = Long.parseLong(p[1].trim());
+                        long currentLastModified = Files.getLastModifiedTime(sourceFile).toMillis();
+                        return currentLastModified != lastKnownTime;
+                    } catch (NumberFormatException e){
+                        return true; // if corrupted timestamp then must be changed
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading manifest: " + e.getMessage());
+        }
+        // file not found so return true! as must be new file
+        return true;
+    }
+
+
+}
