f873a1f003bae77b61b446032b7efdf4fc5a0189
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-01-18 15:27:37 +0530

    Added backupmanager object and ran performBackup

--- FileBackupUtility/FileBackupUtility/src/Main.java
+++ FileBackupUtility/FileBackupUtility/src/Main.java
@@ -1,5 +1,12 @@
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Project Name -- File Backup Utility");
+        BackupManager backupManager = new BackupManager("data/manifestFile.txt");
+        backupManager.performBackup(
+                Path.of("test-source"),
+                Path.of("test-backup")
+        );
     }
 }
