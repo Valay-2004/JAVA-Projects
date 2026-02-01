7a75a4971a7627a114f2affd675c4dbdac60da83
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-01 16:40:27 +0530

    Updated perform backup now it will use either zip or folder backup

--- FileBackupUtility/FileBackupUtility/src/BackupManager.java
+++ FileBackupUtility/FileBackupUtility/src/BackupManager.java
@@ -15,39 +15,19 @@ public class BackupManager {
     }
 
     // ====== MAIN BACKUP LOGIC ===== //
-    public void performBackup(Path sourceDir, Path backupDir) {
-        System.out.println("Scanning for changes in: " + sourceDir);
-        try {
-            // to ensure that the directory exists
-            Files.createDirectories(backupDir);
-            AtomicLong changeCount = new AtomicLong();
-
-            //Walk all files in sourceDir
-            // 1. Scan all files in sourceDir
-            // 2. For each file:
-            //      if hasFileChanged(file) => copy it + updateManifest
-            Files.walk(sourceDir)
-                    .filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        try {
-                            if (manifestManager.hasFileChanged(file)) {
-                                Path relPath = sourceDir.relativize(file);
-                                System.out.println("Backing up: " + relPath);
-
-                                fileCopier.copyFile(file, sourceDir, backupDir);
-                                manifestManager.updateManifest(file);
-                                changeCount.getAndIncrement();
-                            }
-                        } catch (IOException e) {
-                            System.err.println("Error processing " + file + ": " + e.getMessage());
-                        }
-                    });
-
-            if (changeCount.get() == 0) System.out.println("No changes detected. Backup Skipped.");
-            else System.out.println("Backup completed! " + changeCount.get() + " file(s) updated");
+    public void performBackup(Path sourceDir, Path backupDir, String mode) {
+        try{
+            if("zip".equals(mode)){
+                System.out.println("📁 Using ZIP backup mode");
+                // ZipBackupWriter will be added later here
+            } else {
+                System.out.println("📂 Using folder backup mode");
+                FolderBackupWriter writer = new FolderBackupWriter();
+                writer.backup(sourceDir, backupDir, manifestManager);
+            }
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("⚠️ Backup failed ", e.getCause());
         }
     }
 }
