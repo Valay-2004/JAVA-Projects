309748a4ac67be5e656de9fedc7c530156df2920
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-01 17:35:56 +0530

    Final changes -- finishing the Project

--- FileBackupUtility/FileBackupUtility/src/Main.java
+++ FileBackupUtility/FileBackupUtility/src/Main.java
@@ -3,10 +3,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\t\tProject Name -- File Backup Utility");
+        System.out.println("-----------------------------------------------------");
         try {
+            // Load config
             BackupConfig config = new BackupConfig("backup.properties");
+            // Get user's choice
+            BackupModeSelector selector = new BackupModeSelector();
+            String mode = selector.selectMode();
+            // Run backup
             BackupManager backupManager = new BackupManager("data/manifest.txt");
-            backupManager.performBackup(config.sourceDir, config.backupDir, config.backupMode);
+            backupManager.performBackup(config.sourceDir, config.backupDir, mode);
         } catch (IOException e) {
             System.err.println("⚠ Config error: " + e.getMessage());
             System.exit(1);
