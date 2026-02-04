d78f8faed2db78d62946387abc8c45fbd1360f81
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-04 18:31:02 +0530

    Initial Commit -- Start of the Mini Project

--- InventoryManagementSystem/src/Main.java
+++ InventoryManagementSystem/src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args){
-
+        System.out.println("------------------------------------------------------------");
+        System.out.print("\t\tProject name -- Inventory Management System\n");
+        System.out.println("------------------------------------------------------------");
     }
 }
