c124949ce3a2ed23387614bd0c8e91c6966f7c55
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-10 21:09:33 +0530

    Variables and Constructor

---
+++ InventoryManagementSystem/src/Product.java
@@ -0,0 +1,21 @@
+import java.math.BigDecimal;
+
+public class Product {
+    private String id;
+    private String name;
+    private BigDecimal price;
+    private int stock;
+    private String supplierId;
+    private String categoryId;
+
+    public Product(String id, String name, BigDecimal price, int stock, String supplierId, String categoryId){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.supplierId = supplierId;
+        this.categoryId = categoryId;
+    }
+
+
+}
