7266ed01fe7bbd2b22e4ee0f9778c6ea23c61e77
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-13 22:08:42 +0530

    Add; fields and constructor with methods

--- InventoryManagementSystem/src/service/Inventory.java
+++ InventoryManagementSystem/src/service/Inventory.java
@@ -5,4 +5,25 @@ import model.*;
 import java.util.*;
 
 public class Inventory {
+
+    // Storage Collection (model fields)
+    private Map<String, Product> productsMap;
+    private Map<String, Category> categoryMap;
+    private Map<String, Supplier> supplierMap;
+
+    // Constructor with new instance of HashMaps
+    public Inventory(){
+        this.productsMap = new HashMap<>();
+        this.categoryMap = new HashMap<>();
+        this.supplierMap = new HashMap<>();
+    }
+
+    // methods to register categories & suppliers first
+    public void addCategory(Category category){
+        categoryMap.put(category.getId(), category);
+    }
+    public void addSupplier(Supplier supplier){
+        supplierMap.put(supplier.getId(), supplier);
+    }
+
 }
