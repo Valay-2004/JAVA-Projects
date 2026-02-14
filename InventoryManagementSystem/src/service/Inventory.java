cc542f8d361d543166d463025c5d8c82c593d3bd
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-14 19:24:33 +0530

    Validation complete for addProduct

--- InventoryManagementSystem/src/service/Inventory.java
+++ InventoryManagementSystem/src/service/Inventory.java
@@ -2,6 +2,8 @@ package service;
 
 // imports here
 import model.*;
+
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Inventory {
@@ -29,11 +31,28 @@ public class Inventory {
     // Method for adding Product
     public void addProduct(Product product) throws InvalidProductException{
         // TODO: Validate product != null
+        if(product == null){
+            throw new InvalidProductException("Product cannot be null!");
+        }
         // TODO: Validate product ID is not already used
+        if(productsMap.containsKey(product.getId())){
+            throw new InvalidProductException("Product ID '" + product.getId() + "' is already in use!");
+        }
         // TODO: Validate category exists
+        if(!categoryMap.containsKey(product.getCategoryId())){
+            throw new InvalidProductException("Category ID '" + product.getCategoryId() + "' does not exists!");
+        }
         // TODO: Validate supplier exists
+        if(!supplierMap.containsKey(product.getSupplierId())){
+            throw new InvalidProductException("Supplier ID '" + product.getSupplierId() + "' does not exists!");
+        }
         // TODO: Validate price > 0 and stock >= 0
+        if(product.getPrice().compareTo(BigDecimal.ZERO) > 0){
+            throw new InvalidProductException("Product Price cannot be 0 or less than 0");
+        }
+        if(product.getStock() < 0) throw new InvalidProductException("Stock cannot be less than 0");
 
+        // All checks passed
         productsMap.put(product.getId(), product);
     }
 
