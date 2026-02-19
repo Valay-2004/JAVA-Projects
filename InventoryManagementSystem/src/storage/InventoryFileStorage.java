916d4e900ba8f542b671b0197d1e34a4f8c8db61
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-19 20:40:30 +0530

    Add; saveInventory adn loadInventory methods with ObjectInputStream and ObjectOutputStream

--- InventoryManagementSystem/src/storage/InventoryFileStorage.java
+++ InventoryManagementSystem/src/storage/InventoryFileStorage.java
@@ -1,4 +1,31 @@
 package storage;
 
+import service.Inventory;
+
+import java.io.*;
+
 public class InventoryFileStorage {
+    // static file name
+    private static final String DEFAULT_FILE = "inventory.dat";
+
+    // save Inventory method
+    public void saveInventory(Inventory inventory) throws IOException{
+        try(ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream(DEFAULT_FILE))){
+            oos.writeObject(inventory);
+        }
+    }
+
+    // load inventory method
+    public Inventory loadInventory() throws IOException, ClassNotFoundException{
+        File file = new File(DEFAULT_FILE);
+        if(!file.exists()){
+            // first run ? return empty inventory
+            return new Inventory();
+        }
+        try(ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream(DEFAULT_FILE))){
+            return (Inventory) ois.readObject();
+        }
+    }
 }
