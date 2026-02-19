3be1bec65bc39d395391bdd8a56a2bab655d5cb6
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-19 20:27:08 +0530

    Added a try-catch block for catching any exception while starting the application

--- InventoryManagementSystem/src/Main.java
+++ InventoryManagementSystem/src/Main.java
@@ -1,8 +1,15 @@
+import service.InvalidProductException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
         System.out.print("\t\tProject name -- Inventory Management System\n");
-        System.out.println("------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
+        try {
+            IMSApp.start();
+        } catch (InvalidProductException e) {
+            System.err.println("Error during startup: " + e.getMessage());
+            // Optionally: log, exit, or show user-friendly message
+        }
     }
 }
