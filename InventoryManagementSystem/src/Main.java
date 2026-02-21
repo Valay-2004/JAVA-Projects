e2131c8df5ec791fd96811c9a9c7d534c62db369
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-21 16:18:10 +0530

    Removed try/catch block not needed

--- InventoryManagementSystem/src/Main.java
+++ InventoryManagementSystem/src/Main.java
@@ -1,15 +1,9 @@
-import service.InvalidProductException;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------------------------------");
         System.out.print("\t\tProject name -- Inventory Management System\n");
         System.out.println("-------------------------------------------------------------------");
-        try {
-            IMSApp.start();
-        } catch (InvalidProductException e) {
-            System.err.println("Error during startup: " + e.getMessage());
-            // Optionally: log, exit, or show user-friendly message
-        }
+        IMSApp.start();
     }
 }
