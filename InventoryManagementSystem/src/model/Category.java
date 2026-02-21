14af3f49cb2ac1ebf45c0820b05278afcc5b83c1
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-02-21 16:16:57 +0530

    Add; serializable variable and implemented serializable

--- InventoryManagementSystem/src/model/Category.java
+++ InventoryManagementSystem/src/model/Category.java
@@ -1,6 +1,7 @@
 package model;
-
-public class Category {
+import java.io.Serializable;
+public class Category implements Serializable{
+    private static final long serialVersionUID = 1L;
     private String id;
     private String name;
 
