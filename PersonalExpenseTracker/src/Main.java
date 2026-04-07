d4136dd7b5f73a143cc67fdc56f748f7581fda2d
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-04-07 21:58:08 +0530

    -- Initial Commit --

--- PersonalExpenseTracker/src/Main.java
+++ PersonalExpenseTracker/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        /// Main start of the project will be here
-        /// TODO: Start the system design and implement is little by little
+        // Main start of the project will be here
+        // TODO: Start the system design and implement is little by little
 
     }
 }
