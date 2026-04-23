b49c1de65e9a834aa24ea60767bb1330eb0a1edd
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-04-23 16:48:32 +0530

    Uppercased enum types

--- PersonalExpenseTracker/src/model/CategoryType.java
+++ PersonalExpenseTracker/src/model/CategoryType.java
@@ -1,5 +1,5 @@
 package model;
 
 public enum CategoryType {
-    System, User
+    SYSTEM, USER
 }
--- PersonalExpenseTracker/src/model/TransactionType.java
+++ PersonalExpenseTracker/src/model/TransactionType.java
@@ -1,5 +1,5 @@
 package model;
 
 public enum TransactionType {
-    Income, Expense
+    INCOME, EXPENSE
 }
