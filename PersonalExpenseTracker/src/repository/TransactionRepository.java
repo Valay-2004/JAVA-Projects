73fc48cea7ea48fc177eda47ac2d5a50ccc939e6
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-04-26 21:25:41 +0530

    Completed: Todo list

--- PersonalExpenseTracker/src/repository/TransactionRepository.java
+++ PersonalExpenseTracker/src/repository/TransactionRepository.java
@@ -3,6 +3,7 @@ package repository;
 import com.google.gson.Gson;
 import model.Transaction;
 import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,5 +49,35 @@ public class TransactionRepository {
     }
 
     // TODO: Implement loadAllTransactions() -> List<Transaction>
+    public void loadAllTransactions() {
+        List<Transaction> transactionsList = new ArrayList<>();
+
+        // parse file line by line and return the List of Transactions
+        try (BufferedReader br = new BufferedReader(new FileReader("data/transaction.jsonl"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Transaction transaction = gson.fromJson(line, (Type) Transaction.class);
+                transactionsList.add(transaction);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        transactionsList.forEach(System.out::println);
+    }
+
     // TODO: Implement private atomicWrite(String content, String path)
+    public void atomicWrite(String content, String path){
+        // target path
+        Path targetPath = Paths.get(path);
+        // temp path
+        Path tempPath = Paths.get(path + ".tmp");
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path + ".tmp"))){
+            bw.write(content);
+            Files.move(tempPath, targetPath, StandardCopyOption.ATOMIC_MOVE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
