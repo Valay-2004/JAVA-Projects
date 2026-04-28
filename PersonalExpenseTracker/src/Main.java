3b01c12076875a5721b7396aaf389a214e5bde12
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-04-28 16:34:30 +0530

    TRIAL: tried adding categories and duplicate categories work correctly

--- PersonalExpenseTracker/src/Main.java
+++ PersonalExpenseTracker/src/Main.java
@@ -1,5 +1,8 @@
+import model.Category;
+import model.CategoryType;
 import model.Transaction;
 import model.TransactionType;
+import repository.CategoryRepository;
 import repository.TransactionRepository;
 
 import java.math.BigDecimal;
@@ -9,28 +12,40 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
         // Main start of the project will be here
-        // TODO: Start the system design and implement is little by little
 
-        // Transaction Trial
-        TransactionRepository repo = new TransactionRepository();
+//        // Transaction Trial
+//        TransactionRepository repo = new TransactionRepository();
+//
+//        // new transaction
+//        Transaction t = new Transaction(
+//                null, // auto-generate ID
+//                new BigDecimal("25.50"),
+//                LocalDate.now(),
+//                "cat-123",
+//                TransactionType.EXPENSE,
+//                "Lunch"
+//        );
+//
+//        System.out.println("Please wait!!");
+//        System.out.println("Saving transaction...");
+//        repo.saveTransaction(t);
+//
+//        System.out.println("Loading transaction...");
+//        List<Transaction> loaded = repo.loadAllTransactions();
+//        System.out.println("Loaded " + loaded.size() + " transactions");
+//        loaded.forEach(System.out::println);
 
-        // new transaction
-        Transaction t = new Transaction(
-                null, // auto-generate ID
-                new BigDecimal("25.50"),
-                LocalDate.now(),
-                "cat-123",
-                TransactionType.EXPENSE,
-                "Lunch"
-        );
+        CategoryRepository catRepo = new CategoryRepository();
+        //Load and print default cats
+        List<Category> cats = catRepo.loadCategories();
+        cats.forEach(c -> System.out.println(c.getName() + " (parent: " + c.getParentId() + ")"));
 
-        System.out.println("Please wait!!");
-        System.out.println("Saving transaction...");
-        repo.saveTransaction(t);
+// First add
+        Category gym1 = new Category("cat-gym-test", "Gym", null, CategoryType.USER);
+        System.out.println("First add: " + catRepo.addCategory(gym1)); // true
 
-        System.out.println("Loading transaction...");
-        List<Transaction> loaded = repo.loadAllTransactions();
-        System.out.println("Loaded " + loaded.size() + " transactions");
-        loaded.forEach(System.out::println);
+// Second add with same ID
+        Category gym2 = new Category("cat-gym-test", "Gym Duplicate", null, CategoryType.USER);
+        System.out.println("Second add: " + catRepo.addCategory(gym2)); // false (duplicate ID)
     }
 }
