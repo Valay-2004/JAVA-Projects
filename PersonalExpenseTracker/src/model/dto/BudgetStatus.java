ad69f45c715d31237de60bd39751e09e47ed478b
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-05-03 19:05:10 +0530

    Remove: removed final from all variables for gson to serialize properly

--- PersonalExpenseTracker/src/model/dto/BudgetStatus.java
+++ PersonalExpenseTracker/src/model/dto/BudgetStatus.java
@@ -5,9 +5,9 @@ import model.Budget;
 import java.math.BigDecimal;
 
 public class BudgetStatus {
-    private final Budget budget;
-    private final BigDecimal spent;
-    private final BigDecimal remaining;
+    private Budget budget;
+    private BigDecimal spent;
+    private BigDecimal remaining;
 
     public BudgetStatus(Budget budget, BigDecimal spent, BigDecimal remaining) {
         this.budget = budget;
