e0a19c81fb437e8bb8d7b124d999f1f25904b063
Valay-2004 <178009633+Valay-2004@users.noreply.github.com>
2026-06-21 18:03:06 +0530

    Check: Checking if all classes are good to go (toString() will be added later)

--- LibraryBookManagement/src/Main.java
+++ LibraryBookManagement/src/Main.java
@@ -1,4 +1,41 @@
+import model.*;
+
 void main(){
     // Start of the new project
     // Todo: start modeling the outline of the project
+
+    Author author0 = new Author("100", "F. Scott Fitzgerald");
+    Author author1 = new Author("101", "Le Grand Meaulnes");
+    Books newBook = new Books(
+            "123",
+            "The Great Gatsby",
+            "9780743273565",
+            List.of("auth-001", "auth-002")
+    );
+
+    Patron patron = new Patron("001", "Qwen", "qwen@qwen.com", 10);
+
+    Loan loan = new Loan("00001", "123", "001", LocalDate.parse("2026-06-21"), LocalDate.parse("2026-06-24"), LocalDate.parse("2026-06-30"));
+
+
+    System.out.println("------------------------------------------");
+    System.out.println("Authors: ");
+    System.out.println(author0.getId() + " " + author0.getName());
+    System.out.println(author1.getId() + " " + author1.getName());
+
+    System.out.println("---------------------------------------");
+    System.out.println("Books: ");
+    System.out.println("Title: " + newBook.getTitle() + ", ISBN: " + newBook.getIsbn());
+
+    System.out.println("---------------------------------------");
+    System.out.println("Patrons: ");
+    System.out.println("Patrons id: " + patron.getId() + ", Name: " + patron.getName());
+
+    System.out.println("---------------------------------------");
+    System.out.println("Loans: ");
+    System.out.println("Loan id: " + loan.getId() + ", Patron ID: " + loan.getPatronId() +", Loan date: " + loan.getLoanDate() + ", return date: " + loan.getReturnDate());
+
+    System.out.println("--------------------------------------");
+
+    System.out.println("I don't know how to automate all this so I manually console logged everything!");
 }
